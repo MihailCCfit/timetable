4e46a15e7e71efd753cd768e1a00bc9d306df9db
MihailCCFit <m.tsukanov@g.nsu.ru>
2025-05-19 07:43:07 +0700

    add info

--- src/main/java/org/timetable/algorithm/scheduling/AlgorithmScheduler.java
+++ src/main/java/org/timetable/algorithm/scheduling/AlgorithmScheduler.java
@@ -8,6 +8,13 @@ import java.util.List;
 
 public interface AlgorithmScheduler {
 
+    /**
+     *
+     * @param studyPlanEvolves - studyPlans
+     * @param audiences - all audiences
+     * @param tableTimeSetting - time setting (days and time in a day)
+     * @return few (one or more) results - schedules with penalties if its exist
+     */
     List<ScheduleResult> algorithm(List<StudyPlanEvolve> studyPlanEvolves,
                                    List<AudienceEvolve> audiences,
                                    TableTimeSetting tableTimeSetting);
